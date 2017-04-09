55faeb5f0f26dbf4252f0b36e82b8d3cdcedfe2c
Argaditya RS <Argaditya RS>
2017-04-09 17:27:34 +0700

    Heroku config

--- config/database.php
+++ config/database.php
@@ -81,10 +81,10 @@ return [
 
         'heroku' => [  
             'driver'   => 'mysql',
-            'host'     => parse_url(getenv("DATABASE_URL"))["host"],
-            'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
-            'username' => parse_url(getenv("DATABASE_URL"))["user"],
-            'password' => parse_url(getenv("DATABASE_URL"))["pass"],
+            'host'     => parse_url(getenv("CLEARDB_DATABASE_URL"))["host"],
+            'database' => substr(parse_url(getenv("CLEARDB_DATABASE_URL"))["path"], 1),
+            'username' => parse_url(getenv("CLEARDB_DATABASE_URL"))["user"],
+            'password' => parse_url(getenv("CLEARDB_DATABASE_URL"))["pass"],
             'charset'  => 'utf8',
             'prefix'   => '',
             'schema'   => 'public',
