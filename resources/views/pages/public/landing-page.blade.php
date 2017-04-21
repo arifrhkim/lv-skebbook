c906feebb3a57d6d9db829fe60994a738b19f717
arifrhkim <arif.judicious@gmail.com>
2017-04-21 14:47:27 +0700

    Update landing middle.

--- resources/views/pages/public/landing-page.blade.php
+++ resources/views/pages/public/landing-page.blade.php
@@ -3,70 +3,6 @@
 @section('content')
 
 <section id="featured-slider">
-	{{-- <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-		<!-- Indicators -->
-		<ol class="carousel-indicators">
-			<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-			<li data-target="#carousel-example-generic" data-slide-to="1"></li>
-			<li data-target="#carousel-example-generic" data-slide-to="2"></li>
-			<li data-target="#carousel-example-generic" data-slide-to="3"></li>
-		</ol>
-
-		<!-- Wrapper for slides -->
-		<div class="carousel-inner" role="listbox">
-			<div class="item active" id="item-1">
-				<img src="{{ asset('images/web/background-carousel.jpg') }}" alt="1">
-				<div class="carousel-caption">
-					<h1>30 Hari Membuka <br>Imajinasi Baru</h1>
-					<p>Temukan referensi buku menarik yang dapat mengembangkan potensi & kemampuan kamu.</p>
-					<a href="#" class="btn btn-info btn-rounded">Cari Tau  <i class="fa fa-angle-right" aria-hidden="true"></i></a>
-				</div>
-			</div>
-			<div class="item" id="item-2">
-				<img src="{{ asset('images/web/background-carousel2.jpg') }}" alt="2">
-				<div class="carousel-caption">
-					<div class="row">
-						<div class="col-sm-6 text-left">
-							<h1>Pilihan buku terbaik untuk kamu bulan ini!</h1>
-							<p>20 buku terbaik edisi editor Skeb Book, Mulai dari buku nasional sampai internasional!</p>
-							<p>Dapatkan juga diskon 20%</p>
-							<a href="#" class="btn btn-info btn-rounded">Cari Tau  <i class="fa fa-angle-right" aria-hidden="true"></i></a>
-						</div>	
-					</div>
-				</div>
-			</div>
-			<div class="item" id="item-3">
-				<img src="{{ asset('images/web/background-carousel3.jpg') }}" alt="3">
-				<div class="carousel-caption">
-					<h1>Gak perlu lagi repot <br>hunting buku bekas.</h1>
-					<p>Sekarang ke pasar buku cukup buka skeb book, pilihan buku yang kamu <br> suka langsung dari pedagang buku terpercaya!</p>
-					<a href="#" class="btn btn-info btn-rounded">Cari Tau  <i class="fa fa-angle-right" aria-hidden="true"></i></a>
-				</div>
-			</div>
-			<div class="item" id="item-4">
-				<img src="{{ asset('images/web/background-carousel4.jpg') }}" alt="4">
-				<div class="carousel-caption">
-					<div class="row">
-						<div class="col-sm-6 text-left">
-							<h1>Sekarang kamu bisa <br> <span> menyewakan koleksi <br> buku dirumah</span> tanpa <br> harus khawatir</h1>
-							<p>Kunjungi : loops.skebbook.com</p>
-							<a href="#" class="btn btn-info btn-rounded">Cari Tau  <i class="fa fa-angle-right" aria-hidden="true"></i></a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<!-- Controls -->
-		<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-			<span class="sr-only">Previous</span>
-		</a>
-		<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-			<span class="sr-only">Next</span>
-		</a>
-	</div> --}}
 	<a href="#"><img src="{{ url('images/web/Background.jpg') }}"></a>
 </section>
 
@@ -89,48 +25,33 @@
 </section>
 
 <section id="featured-category" class="container-fluid container-addon">
-
-		<div class="row">
-			<div class="col-md-2 col-lg-2">
-				<div class="panel panel-default">
-					<div class="panel-body">
-						Kuliner & travel
-					</div>
+	<div class="row">
+			<div class="col-sm-3 col-xs-12">
+			    <div class="book">
+				    <a href="books">
+					    <img src="{{ asset('images/web/Block1.jpg') }}" class="img-responsive book-image">
+					</a>
 				</div>
 			</div>
-			<div class="col-md-2 col-lg-2">
-				<div class="panel panel-default">
-					<div class="panel-body">
-						Buku anak anak
-					</div>
+			<div class="col-sm-3 col-xs-12">
+				<div class="book">
+				    <a href="books">
+					    <img src="{{ asset('images/web/block2.jpg') }}" class="img-responsive book-image">
+					</a>
 				</div>
 			</div>
-			<div class="col-md-2 col-lg-2">
-				<div class="panel panel-default">
-					<div class="panel-body">
-						Majalah & Tabloid
-					</div>
+			<div class="col-sm-3 col-xs-12">
+				<div class="book">
+				    <a href="books">
+					    <img src="{{ asset('images/web/block3.jpg') }}" class="img-responsive book-image">
+					</a>
 				</div>
 			</div>
-			<div class="col-md-2 col-lg-2">
-				<div class="panel panel-default">
-					<div class="panel-body">
-						Seni & Fotografi
-					</div>
-				</div>
-			</div>
-			<div class="col-md-2 col-lg-2">
-				<div class="panel panel-default">
-					<div class="panel-body">
-						Bisnis & Manajemen
-					</div>
-				</div>
-			</div>
-			<div class="col-md-2 col-lg-2">
-				<div class="panel panel-default">
-					<div class="panel-body">
-						Biografi & sejarah
-					</div>
+			<div class="col-sm-3 col-xs-12">
+				<div class="book">
+				    <a href="books">
+					    <img src="{{ asset('images/web/block4.jpg') }}" class="img-responsive book-image">
+					</a>
 				</div>
 			</div>
 		</div>
@@ -141,9 +62,20 @@
 		<div class="col-md-4">
 			<div id="sejarah">
 				<div class="img-container">
-					<h1>Saatnya memutar waktu.</h1>
-					<p>Masuk kedalam dimensi dimana kamu dapat mengetahui tentang sejarah dan cerita yang kamu suka</p>
-					<a href="#" class="btn btn-outline btn-rounded">Cari Tau <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+					<h1>Tidak menemukan buku yang dicari?</h1>
+					<p>Biarkan ratusan penjual buku membantu, lelang pencarian buku sekarang.</p>
+					<form >
+						<div class="form-group">
+						  <input type="text" class="form-control" placeholder="Judul Buku">
+						</div>
+						<div class="form-group">
+						  <input type="text" class="form-control" placeholder="Nama penulis / penerbit">
+						</div>
+						<div class="form-group">
+						  <input type="date" class="form-control" placeholder="Pilih tanggal deadline lelang">
+						</div>
+						<a type="submit" class="btn btn-outline btn-rounded">Bantu Cari</a>
+					</form>
 				</div>
 			</div>
 		</div>
@@ -152,27 +84,37 @@
 				<div class="col-md-6">
 					<div id="komik">
 						<div class="img-container">
-							<h1>Komik setiap hari!</h1>
-							<p>Rasakan pengalaman baru mencari komik kesayangan kamu</p>
-							<a href="#" class="btn btn-outline btn-rounded">Cari Tau <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+							<h1>Tunjukan Karyamu</h1>
+							<p>Publikasi bukumu sendiri, dapatkan fans page untuk berinteraksi dan mengapresiasi kryamu.</p>
+							<a href="#" class="btn btn-outline btn-rounded">Buat halaman<i class="fa fa-angle-right" aria-hidden="true"></i></a>
 						</div>
 					</div>
 				</div>
 				<div class="col-md-6">
 					<div id="karya">
 						<div class="img-container">
-							<h1>Totalitas berkarya</h1>
-							<p>Dukung dan kembangkan passion kamu, baca buku yang kamu suka sekarang</p>
-							<a href="#" class="btn btn-outline btn-rounded">Cari Tau <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+							<h1>E-book dari penulis keren</h1>
+							<p>Koleksi dari buku e-book terbaru mulai dari penulis terkenal sampai penulis independen.</p>
+							<a href="#" class="btn btn-outline btn-rounded">Cari e-book <i class="fa fa-angle-right" aria-hidden="true"></i></a>
 						</div>
 					</div>
 				</div>
 				<div class="col-md-12">
 					<div id="pengalaman">
 						<div class="img-container">
-							<h1>Jelajahi Pengalaman Baru</h1>
-							<p>Temukan buku yang pas dengan mood kamu minggu ini</p>
-							<a href="#" class="btn btn-outline btn-rounded">Cari Tau <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+							<h1>Langganan majalah favorite kamu.</h1>
+							<div class="form-group">
+								<select class="form-control" placeholder="Pilih majalah">
+								  <option>Pilih merk majalah</option>
+								  <option>CHIP</option>
+								  <option>Gadis</option>
+								  <option>Tempo</option>
+								  <option>Hai</option>
+								  <option>Bobo</option>
+								  <option>Mentari</option>
+								</select>
+							</div>
+							<a href="#" class="btn btn-outline btn-rounded">Langganan Majalah <i class="fa fa-angle-right" aria-hidden="true"></i></a>
 						</div>
 					</div>
 				</div>
@@ -181,18 +123,151 @@
 	</div>
 </section>
 
-<section id="popular-product" class="container-fluid container-addon">
-	<div class="row">
-		<div class="col-sm-12"><h2>Buku Populer</h2><hr></div>
-		@for ($i=0; $i<4; $i++)
-		<div class="col-sm-3 col-xs-12">
-			<div class="book">
-				<img src="{{ asset('images/web/buku.png') }}" class="img-responsive book-image">
-				<h3 class="book-title ellipsis">Novel Drama</h3>
-				<p class="book-price ellipsis">Rp 35.000,-</p>
-			</div>
+<section id="product-popular" class="">
+    <div class="container">
+        <div class="page-header">
+        	<h3>Buku Populer
+        		<div class="pull-right">
+        			<!-- Left and right controls -->
+        			<a class="btn btn-danger btn-rounded" href="#populerCarousel" role="button" data-slide="prev">
+        				<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        			</a>
+        			<a class="btn btn-danger btn-rounded" href="#populerCarousel" role="button" data-slide="next">
+        				<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        			</a>
+        		</div>
+        	</h3>
+        </div>
+
+        <div id="populerCarousel" class="carousel slide" data-ride="carousel">
+          <!-- Wrapper for slides -->
+          <div class="carousel-inner" role="listbox">
+                <div class="item active">
+        			<div class="row">
+        			    @for ($i=0; $i<4; $i++)
+            			  <div class="col-sm-6 col-md-3">
+            			    <div class="thumbnail">
+            						<a href="detail.html">
+            			      	<img class="img-responsive" src="{{ asset('images/web/buku.png') }}" alt="buku1">
+            						</a>
+            			    	<div class="caption">
+            							<h5 id="judul">
+            								Book Number 1
+            								<div id="hearts" class="starrr pull-right"></div>
+            							</h5>
+            							<a href="detail.html">
+            				    		<h4>Novel Drama</h4>
+            							</a>
+            				    	<h4 id="harga">Rp. 35.000,-</h4>
+            			    	</div>
+            			    </div>
+            			   </div>
+                        @endfor
+            		</div>
+                </div>
+
+            <div class="item">
+        			<div class="row">
+        			  @for ($i=0; $i<4; $i++)
+            			  <div class="col-sm-6 col-md-3">
+            			    <div class="thumbnail">
+            						<a href="detail.html">
+            			      	<img class="img-responsive" src="{{ asset('images/web/buku.png') }}" alt="buku1">
+            						</a>
+            			    	<div class="caption">
+            							<h5 id="judul">
+            								Book Number 1
+            								<div id="hearts" class="starrr pull-right"></div>
+            							</h5>
+            							<a href="detail.html">
+            				    		<h4>Novel Drama</h4>
+            							</a>
+            				    	<h4 id="harga">Rp. 35.000,-</h4>
+            			    	</div>
+            			    </div>
+            			   </div>
+            		    @endfor
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</section>
+
+<section id="product-newest" class="">
+<div class="container">
+    <!-- Terbaru -->
+<div class="page-header">
+	<h3>Buku Terbaru
+		<div class="pull-right">
+
+			<!-- Left and right controls -->
+			<a class="btn btn-danger btn-rounded" href="#newestCarousel" role="button" data-slide="prev">
+				<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+			</a>
+			<a class="btn btn-danger btn-rounded" href="#newestCarousel" role="button" data-slide="next">
+				<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+			</a>
+
 		</div>
-		@endfor
+	</h3>
+</div>
+
+<div id="newestCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+
+			<div class="row">
+			    @for ($i=0; $i<4; $i++)
+    			  <div class="col-sm-6 col-md-3">
+    			    <div class="thumbnail">
+    						<a href="detail.html">
+    			      	<img class="img-responsive" src="{{ asset('images/web/buku.png') }}" alt="buku1">
+    						</a>
+    			    	<div class="caption">
+    							<h5 id="judul">
+    								Book Number 1
+    								<div id="hearts" class="starrr pull-right"></div>
+    							</h5>
+    							<a href="detail.html">
+    				    		<h4>Novel Drama</h4>
+    							</a>
+    				    	<h4 id="harga">Rp. 35.000,-</h4>
+    			    	</div>
+    			    </div>
+    			   </div>
+    			  @endfor
+			</div>
+    </div>
+
+    <div class="item">
+
+			<div class="row">
+
+			  @for ($i=0; $i<4; $i++)
+    			  <div class="col-sm-6 col-md-3">
+    			    <div class="thumbnail">
+    						<a href="detail.html">
+    			      	<img class="img-responsive" src="{{ asset('images/web/buku.png') }}" alt="buku1">
+    						</a>
+    			    	<div class="caption">
+    							<h5 id="judul">
+    								Book Number 1
+    								<div id="hearts" class="starrr pull-right"></div>
+    							</h5>
+    							<a href="detail.html">
+    				    		<h4>Novel Drama</h4>
+    							</a>
+    				    	<h4 id="harga">Rp. 35.000,-</h4>
+    			    	</div>
+    			    </div>
+    			   </div>
+    			  @endfor
+					</div>
+		    </div>
+		  </div>
+		</div>
 	</div>
 </section>
 
